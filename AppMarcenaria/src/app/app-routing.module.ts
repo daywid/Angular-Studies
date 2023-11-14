82a1d4dccc8203abe247125687c15237d756f012
Daywid <88506180+daywid@users.noreply.github.com>
2023-11-13 21:56:04 -0300

    almost done

--- AppMarcenaria/src/app/app-routing.module.ts
+++ AppMarcenaria/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgendasComponent } from './components/agendas/agendas.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'agendas', component:AgendasComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
