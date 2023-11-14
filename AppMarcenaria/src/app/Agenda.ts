783082651f62e2551b773747e039d75e91bbe630
Daywid <88506180+daywid@users.noreply.github.com>
2023-11-14 15:36:35 -0300

    Update Agenda.ts

--- AppMarcenaria/src/app/Agenda.ts
+++ AppMarcenaria/src/app/Agenda.ts
@@ -5,26 +5,4 @@ export class Agenda {
   tipo?: string;
   funcionarioId: number = 0;
 
-
-
-  // setDataFromString(dataString: string): void {
-  //   const dateArray = dataString.split('-').map(Number);
-  //   this.data = new Date(dateArray[0], dateArray[1] - 1, dateArray[2]);
-  // }
-
-  // toJSON(): any {
-  //   // Converte a data para o formato desejado.
-  //   const formattedDate = this.data.toISOString().split('T')[0];
-
-  //   return {
-  //     id: this.id,
-  //     descricao: this.descricao,
-  //     data: formattedDate,
-  //     tipo: this.tipo,
-  //     funcionarioId: this.funcionarioId,
-  //   };
-  // }
-
-
-
 }
